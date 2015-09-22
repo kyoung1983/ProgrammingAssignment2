5d454e1c4f9f36188787dad120ac560c52b7168d
kyoung1983 <kyoung@laureateinstitute.org>
2015-09-22 12:01:42 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Write a short comment describing this function - we hold the cached value (initially nothing is cached), store a matrix and return it, cache the argument, get the cahced value and then return a list. Each element in the list is a function
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
@@ -16,7 +16,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this function - we get the cahced value (if one exists return it), otherwise get the matrix, calculate the inverse and store it in the cache. Return the inverse
 
 cacheSolve <- function(x, ...) {
   inv <- x$getinverse()
